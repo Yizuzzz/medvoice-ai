97f02cf5d6dd871ab30807f60d34d7d6c1e455a7
Jesús Alvarez <caminador_buceador.0n@icloud.com>
2026-02-27 23:53:02 -0600

    feat: Measure latency on queries

--- app/repositories/query_repository.py
+++ app/repositories/query_repository.py
@@ -1,6 +1,9 @@
+import time
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
+from app.services.ingestion.embedder import get_embedding
+import uuid
 
 async def search_similar_queries(
         db: AsyncSession,
@@ -27,3 +30,48 @@ async def search_similar_queries(
     )
 
     return result.fetchall()
+
+async def save_query(
+        db: AsyncSession,
+        question: str,
+        response: str,
+        model_used: str,
+):
+    start_time = time.time()
+
+    embedding = get_embedding(question)
+
+    latency_ms = int((time.time() - start_time) * 1000)
+
+    await db.execute(
+        text("""
+            INSERT INTO queries (
+                id,
+                question,
+                embedding,
+                response,
+                model_used,
+                latency_ms,
+                created_at
+            )
+            VALUES (
+                :id,
+                :question,
+                :embeding::vector,
+                :response,
+                :model_used,
+                :latency_ms,
+                NOW()
+            )
+        """),
+        {
+            "id": str(uuid.uuid4()),
+            "question": question,
+            "embeding": embedding,
+            "response": response,
+            "model_used": model_used,
+            "latency_ms": latency_ms
+        }
+    )
+
+    await db.commit()
